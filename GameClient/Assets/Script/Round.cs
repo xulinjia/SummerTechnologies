55c7256d30ccb51ad260c4197f526ae239118fd6
xulinjia <xulinjia@dobest.com>
2019-07-10 20:45:10 +0800

    [程序] 旋转代码

---
+++ GameClient/Assets/Script/Round.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Round : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = transform.localEulerAngles + Vector3.up * Time.deltaTime * 10;
+    }
+}
